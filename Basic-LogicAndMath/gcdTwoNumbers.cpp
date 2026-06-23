18a19163b155812942aba6f845a0edbebe4dcf68
Yashvin <yashvinmehra@gmail.com>
2026-06-23 12:42:42 +0530

    gcd of 2 numbers done

---
+++ Basic-LogicAndMath/gcdTwoNumbers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << findGCD(a, b);
+
+    return 0;
+}
