b3184be44d111abc1e2591f07bda23d79400ac8f
Yashvin <yashvinmehra@gmail.com>
2026-06-26 12:07:33 +0530

    strarted recursion

---
+++ recursion/print1toN.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printNumbers(int i, int n) {
+    if (i > n)
+        return;
+
+    cout << i << " ";
+
+    printNumbers(i + 1, n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printNumbers(1, n);
+
+    return 0;
+}
